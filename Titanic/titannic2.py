f7a58fb8e3eca5e2fc77d59190a25f40173bbeb1
priiyam <crifte@gmail.com>
2018-01-26 21:11:59 +0530

    titanic.py using ANN added

--- Titanic/titannic2.py
+++ Titanic/titannic2.py
@@ -41,5 +41,27 @@ y_pred = (y_pred>0.5)*1
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y, y_pred)
 
+# Test Set
+dataset2 = pd.read_csv("test.csv")
+X2 = dataset2.iloc[:, [1,3,4,5,6,8]].values
+y_id = dataset2.iloc[:, 0].values
+
+from sklearn.preprocessing import Imputer
+imputer2 = Imputer(missing_values="NaN", strategy="mean", axis=0)
+imputer2 = imputer2.fit(X2[:, 2:3])
+X2[:, 2:3] = imputer2.transform(X2[:, 2:3])
+imputer3 = imputer2.fit(X2[:, 5:6])
+X2[:, 5:6] = imputer3.transform(X2[:, 5:6])
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X2 = LabelEncoder()
+X2[:, 1] = labelencoder_X2.fit_transform(X2[:, 1])
+onehotencoder2 = OneHotEncoder( categorical_features=[1] )
+X2 = onehotencoder2.fit_transform(X2).toarray()
+
+from sklearn.preprocessing import StandardScaler
+sc2 = StandardScaler()
+X2 = sc2.fit_transform(X2)
+
 
 
