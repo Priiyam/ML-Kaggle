fdde1798cfbad41d4a15ad0196d3f1dc8f8043f0
priiyam <crifte@gmail.com>
2018-02-03 11:14:52 +0530

    Housing-Prices script.py added

--- Housing-Prices/script.py
+++ Housing-Prices/script.py
@@ -9,5 +9,20 @@ y = np.log(data.SalePrice)
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.25)
 
+from sklearn.linear_model import LinearRegression #0.02192, 0.204, 
+regressor = LinearRegression()
+regressor.fit(X, y)
+
+y_pred = regressor.predict(X_test)
+
+# Visualising and checks
+from sklearn.metrics import mean_squared_error
+print ("RMSE is: \n", mean_squared_error(y_test, y_pred))
+plt.scatter(y_pred, y_test, alpha=.75, color='b') #alpha helps to show overlapping data
+plt.xlabel('Predicted Price')
+plt.ylabel('Actual Price')
+plt.title('Linear Regression Model')
+plt.show()
+
 
 
