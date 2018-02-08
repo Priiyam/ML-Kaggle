9610251df473f67a04d32bf1c2dc80a76fadc83c
priiyam <crifte@gmail.com>
2018-02-08 11:54:40 +0530

    Analyzation added

--- Housing-Prices/analyze.py
+++ Housing-Prices/analyze.py
@@ -10,6 +10,14 @@ test = pd.read_csv('test.csv')
 train.SalePrice.describe()
 plt.hist(train.SalePrice, color="blue") #histogram, + skewed
 
+print ("Skew is: ", (np.log(train.SalePrice)).skew()) # applying log to skewed
+plt.hist(np.log(train.SalePrice), color="blue") # removed skewness, visual
+
+numeric_features = train.select_dtypes(include=[np.number])
+numeric_features.dtypes
+
+plt.scatter(x=train['GarageArea'], y=np.log(train.SalePrice))
+
 
 
 
